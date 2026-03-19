bae19738b0b25decb9cc9aad3b058677c7276a89
Darío <20240167@ricaldone.edu.sv>
2026-03-19 15:39:43 -0600

    4 cruds

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 import express from "express"
 import productRoutes from "./src/routes/products.js"
 import branchesRoutes from "./src/routes/branches.js"
+import employeeRoutes from "./src/routes/employee.js"
+import reviewsRoutes from "./src/routes/reviewsRoutes.js"
 
 const app = express();
 
@@ -8,7 +10,9 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
-app.use("/api/branches", branchesRoutes)
+app.use("/api/branches", branchesRoutes);
+app.use("/api/employees", employeeRoutes);
+app.use("/api/reviews", reviewsRoutes);
 
 
 export default app;
